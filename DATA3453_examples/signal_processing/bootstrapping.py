dba4943cad4e09a562e55c5e954efb44efbbcde5
echalmers <echalmers@mtroyal.ca>
2026-03-17 14:25:30 -0600

    uncertainty code

--- DATA3453_examples/signal_processing/bootstrapping.py
+++ DATA3453_examples/signal_processing/bootstrapping.py
@@ -3,20 +3,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def bootstrap_CI(values, n_resamples, CI_lower, CI_upper):
+def bootstrap_CI(values, n_resamples, CI_lower=2.5, CI_upper=97.5, statistic_to_calculate=np.mean):
     statistics = []
     for resample in range(n_resamples):
         resample = np.random.choice(values, len(values), replace=True) # sample values with replacement
-        statistics.append(np.percentile(resample, 75))
+        statistics.append(statistic_to_calculate(resample))
     return np.percentile(statistics, CI_lower), np.percentile(statistics, CI_upper)
 
-df = pd.read_csv('./DATA3453_examples/plotly_dash/housing_holdout.csv', index_col=None)
 
-p75_price = np.percentile(df['re_assessed_value_2024'], 75)
-lower, upper = bootstrap_CI(df['re_assessed_value_2024'], 1000, 2.5, 97.5)
+if __name__ == '__main__':
+    df = pd.read_csv('./DATA3453_examples/plotly_dash/housing_holdout.csv', index_col=None)
 
-print(p75_price, lower, upper)
+    mean_price = np.mean(df['re_assessed_value_2024'])
+    lower, upper = bootstrap_CI(df['re_assessed_value_2024'], 1000, 2.5, 97.5)
 
-plt.bar(1, p75_price)
-plt.errorbar(x=1, y=p75_price, yerr=[[p75_price - lower], [upper - p75_price]], capsize=10, color='k')
-plt.show()
+    print(mean_price, lower, upper)
+
+    plt.bar(1, mean_price)
+    plt.errorbar(x=1, y=mean_price, yerr=[[mean_price - lower], [upper - mean_price]], capsize=10, color='k')
+    plt.show()
--- DATA3453_examples/signal_processing/showing_uncertainty.py
+++ DATA3453_examples/signal_processing/showing_uncertainty.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.neighbors import KernelDensity
-from bootstrapping import boostrap_CI
+from bootstrapping import bootstrap_CI
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF, WhiteKernel, Matern, RationalQuadratic
 
@@ -17,14 +17,54 @@ df['y'] = df['x'] ** 0.8 + np.random.normal(0, 3, n)
 # create a few places with sparse data
 df = df[(df['x'] != 10) & (df['x'] != 20)]
 df = df[(df['x'] < 25) | (df['x'] > 30)]
+df = df[(df['x'] < 7) | (df['x'] > 13)]
 df.index = np.arange(len(df))
 df.loc[len(df), ['x', 'y']] = [10, 10**0.8 + 0.2]
 df.loc[len(df), ['x', 'y']] = [10, 10**0.8 - 0.2]
 df.loc[len(df), ['x', 'y']] = [20, 20 ** 0.8 - 4]
 
 # plot the data
-fig, ax = plt.subplots(2, 1, sharex=True)
-plt.sca(ax[0])
 plt.scatter(df.x, df.y, c=['b']*(len(df)-3) + ['r'] * 3, alpha=[0.1]*(len(df)-3) + [0.5]*3)
 sns.lineplot(df, x='x', y='y', errorbar=('ci', 95))
 plt.title('original data with Seaborn 95% CI')
+
+
+# fit a KDE to the data.
+# YOUR CODE HERE
+
+# plot the KDE estimates of P(x)
+fig, ax = plt.subplots(2, 1, sharex=True)
+# YOUR CODE HERE
+
+# re-create the CI (using bootstrapping), but now divide the CI by KDE estimates of P(x)
+x_vals = df['x'].sort_values().unique()
+y_mean_vals = df.groupby('x')['y'].mean().values
+kde_probability = np.exp(kde.score_samples(x_vals.reshape(-1, 1)))
+kde_probability_normalized = kde_probability / kde_probability.max()
+
+CIs = np.array([bootstrap_CI(df.loc[df['x'] == x, 'y'], n_resamples=100) for x in x_vals])
+# push the upper and lower CI limits away from the mean line
+CIs -= y_mean_vals.reshape(-1, 1)
+CIs[:, 0] /= kde_probability_normalized
+CIs[:, 1] /= kde_probability_normalized
+CIs += y_mean_vals.reshape(-1, 1)
+
+# plot the new CIs
+plt.sca(ax[0])
+plt.scatter(df['x'], df['y'], c='b', alpha=0.2)
+plt.plot(x_vals, y_mean_vals)
+plt.fill_between(x_vals, CIs[:, 0], CIs[:, 1], alpha=0.5)
+
+
+# fit a gaussian process to the data
+kernel = RationalQuadratic(length_scale=1) # RBF(length_scale=2) #+ WhiteKernel(noise_level=10)
+gp = GaussianProcessRegressor(kernel=kernel, normalize_y=True)  # alpha=(df['alpha'].values * 2)**3)
+gp.fit(df[['x']], df[['y']])
+y_mean, y_std = gp.predict(np.linspace(0, 40, 100).reshape(-1, 1), return_std=True)
+
+
+# plot the mean and standard deviation predicted by the guassian process regression
+# YOUR CODE HERE
+
+
+plt.show()
